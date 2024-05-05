f4d10b928030952f7d01cb663779aa46d2acd1fa
Kartik Narang <narangk007@gmail.com>
2024-05-05 20:35:26 +0530

    Build Sidebar Ui

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,15 +1,15 @@
-import React, { Children } from 'react'
+import MenuOptions from "@/components/sidebar";
+import React from "react";
 
-type Props = {children:React.ReactNode}
+type Props = { children: React.ReactNode };
 
 const Layout = (props: Props) => {
   return (
-    <div className='flex overflow-hidden h-screen'>
-        <div className='w-full'>
-            {props.children}
-        </div>
+    <div className="flex overflow-hidden h-screen">
+      <MenuOptions/>
+        <div className="w-full">{props.children}</div>
     </div>
-  )
-}
+  );
+};
 
 export default Layout;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,7 @@ export default function Home() {
   return (
     <main className="flex items-center justify-center flex-col">
       <Navbar />
+      {/* Container - Scroll */}
       <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
         <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
         <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
@@ -41,9 +42,11 @@ export default function Home() {
         direction="right"
         speed="slow"
       />
+      {/* Hero Parallax Section */}
       <section>
         <HeroParallax products={products}></HeroParallax>
       </section>
+      {/* Lamp and Cards */}
       <section className="mt-[-500px]">
         <LampComponent />
         <div className="flex flex-wrap items-center justify-center flex-col md:flex-row gap-8 -mt-72">
